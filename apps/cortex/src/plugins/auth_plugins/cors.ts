9b61877ec06a9135914004f2430764feb996633d
Arav menon <aravmenon.ak@gmail.com>
2026-03-18 03:48:50 +0530

    feat: setup global cors plugin of fastify

---
+++ apps/cortex/src/plugins/auth_plugins/cors.ts
@@ -0,0 +1,10 @@
+import fp from "fastify-plugin";
+import fastifyCors from "@fastify/cors";
+import type { FastifyInstance } from "fastify";
+
+export default fp(async (fastify: FastifyInstance) => {
+    fastify.register(fastifyCors, {
+        origin: process.env.CORS_ORIGIN ?? "*",
+        credentials: true,
+    });
+});
