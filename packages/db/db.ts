9631c1b93c61ba24ca57e550f331c78eeb460157
Arav menon <aravmenon.ak@gmail.com>
2026-03-17 23:48:03 +0530

    feat : change export method

--- packages/db/db.ts
+++ packages/db/db.ts
@@ -4,6 +4,4 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
-const prisma = new PrismaClient({ adapter });
-
-export default prisma;
+export const db = new PrismaClient({ adapter });
