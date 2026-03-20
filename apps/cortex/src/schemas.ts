9ba24b95ea9b24f23f5ae0106dbaf38587c55770
Arav menon <aravmenon.ak@gmail.com>
2026-03-20 16:33:50 +0530

    feat : change global auth schema file name

--- apps/cortex/src/schemas.ts
+++
@@ -1,6 +0,0 @@
-import { buildJsonSchemas } from "fastify-zod";
-import * as authSchemas from "@repo/schemas";
-
-export const { schemas: authJsonSchemas, $ref } = buildJsonSchemas(authSchemas, {
-    $id: "auth",
-});
