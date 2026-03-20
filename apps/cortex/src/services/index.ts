394323dfc97139d6b46664e4757022e4fcbc4f7e
Arav menon <aravmenon.ak@gmail.com>
2026-03-20 16:05:36 +0530

    feat : add global export of services file

---
+++ apps/cortex/src/services/index.ts
@@ -0,0 +1,3 @@
+export * from "./workspace_services/workspace.service";
+export * from "./orgInvite_link_services/orgInvite.services";
+export * from "./orgInvite_accpet_services/accpetInviteService";
