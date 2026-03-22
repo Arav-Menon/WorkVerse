2174c152ebe60b0d07cdd740c4839caa2e872421
Arav menon <aravmenon.ak@gmail.com>
2026-03-22 22:59:37 +0530

    feat : add global export for create room controller

--- apps/cortex/src/controllers/index.ts
+++ apps/cortex/src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from "./organization.controller";
 export * from "./workspace.controller";
 export * from "./orgInvite.controller";
 export * from "./acceptInvitation.controller";
+export * from "./create-room.controller"
