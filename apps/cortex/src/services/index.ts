15bb60f3f2db9ea4fc937c95cd326518def9ca48
Arav menon <aravmenon.ak@gmail.com>
2026-03-22 23:01:48 +0530

    feat : add global export for create room service

--- apps/cortex/src/services/index.ts
+++ apps/cortex/src/services/index.ts
@@ -1,3 +1,4 @@
 export * from "./workspace_services/workspace.service";
 export * from "./orgInvite_link_services/orgInvite.services";
 export * from "./orgInvite_accpet_services/accpetInviteService";
+export * from "./room_services/create-room.service";
