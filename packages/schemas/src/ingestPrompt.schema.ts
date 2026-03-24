4edfde793dca45d1fa2c5ab86562696214f99ed6
Arav menon <aravmenon.ak@gmail.com>
2026-03-24 23:32:43 +0530

    feat: Add new prompt ingestion with defined schemas. and user inbound prompt schema

---
+++ packages/schemas/src/ingestPrompt.schema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const ingestPromptSchema = z.object({
+  promptId: z.string(),
+  userId: z.string(),
+  organizationId: z.string(),
+  workspaceId: z.string(),
+  systemPrompt: z.string(),
+  userPrompt: z.string(),
+});
+
+export const UserInboundPromptSchema = z.object({
+  promptId: z.string(),
+  organizationId: z.string(),
+  userId: z.string(),
+  workspaceId: z.string(),
+  systemPrompt: z.string(),
+  userPrompt: z.string(),
+});
+
+export type ingestPromptBody = z.infer<typeof ingestPromptSchema>;
+export type UserInboundPrompt = z.infer<typeof UserInboundPromptSchema>;
