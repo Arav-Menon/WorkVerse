9740fe357d91a9101608cb2405f11a37ddbd46c9
Arav menon <aravmenon.ak@gmail.com>
2026-03-24 23:31:45 +0530

    feat : global export of ingest prompt schema

--- packages/schemas/index.ts
+++ packages/schemas/index.ts
@@ -2,3 +2,4 @@ export * from "./src/auth.schema";
 export * from "./src/organization.schema";
 export * from "./src/workspace.schema";
 export * from "./src/room.schema";
+export * from "./src/ingestPrompt.schema";
