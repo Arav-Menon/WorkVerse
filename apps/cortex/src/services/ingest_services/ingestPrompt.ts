7a32dfc770278f77e7531f3e27d93ce4b726e727
Arav menon <aravmenon.ak@gmail.com>
2026-03-26 23:52:25 +0530

    feat : setup ingest Prompt to redis streams

--- apps/cortex/src/services/ingest_services/ingestPrompt.ts
+++ apps/cortex/src/services/ingest_services/ingestPrompt.ts
@@ -1,16 +1,19 @@
 import { type FastifyInstance } from "fastify";
 import type { ingestPromptBody } from "@repo/schemas";
+import { tokenId } from "./getId";
+import { aiSystemPrompt } from "./systemPrompt";
 
 export async function registerIngestPrompt(
   fastify: FastifyInstance,
   input: ingestPromptBody,
   userId: string,
 ): Promise<{ success: boolean; id?: string; error?: string }> {
-  const { userPrompt, promptId, organizationId, workspaceId, systemPrompt } =
-    input;
+  const { userPrompt, organizationId, workspaceId } = input;
+
+  const systemPrompt = aiSystemPrompt();
 
   const result = await fastify.redisProducer.pushUserInboundPrompt({
-    promptId,
+    promptId: tokenId(),
     userId,
     organizationId,
     workspaceId,
