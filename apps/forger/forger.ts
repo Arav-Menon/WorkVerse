a91cb14464de6d9143283cea3577be008a444cda
Arav menon <aravmenon.ak@gmail.com>
2026-03-26 23:57:36 +0530

    feat : setup worker for pull user in bound prompt

--- apps/forger/forger.ts
+++ apps/forger/forger.ts
@@ -1,3 +1,12 @@
+import { pullUserInboundPrompt } from "@repo/redis/redis-client";
+
 while (true) {
-    
+  const userInboundPrompt = await pullUserInboundPrompt();
+  if (!userInboundPrompt) continue;
+
+  console.dir(userInboundPrompt, { depth: null });
+
+  const stream = userInboundPrompt.response[0].message[0];
+
+  console.log(stream);
 }
