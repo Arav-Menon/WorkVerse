b02a3f25c0819ae35f5354d4a0766b8eca0613fa
Arav menon <aravmenon.ak@gmail.com>
2026-03-27 23:18:19 +0530

    feat : add forger process with ai  for response

--- apps/forger/forger.ts
+++ apps/forger/forger.ts
@@ -1,12 +1,31 @@
 import { pullUserInboundPrompt } from "@repo/redis/redis-client";
+import { processWithAi } from "@repo/evaluator";
 
 while (true) {
   const userInboundPrompt = await pullUserInboundPrompt();
-  if (!userInboundPrompt) continue;
+  if (!userInboundPrompt || !userInboundPrompt.response) continue;
 
   console.dir(userInboundPrompt, { depth: null });
 
-  const stream = userInboundPrompt.response[0].message[0];
+  const stream = userInboundPrompt.response[0]?.messages[0]?.message;
+  if (!stream) continue;
+
+  console.log(stream);
+
+  const { userId, organizationId, workspaceId, systemPrompt, userPrompt } =
+    stream;
+
+  try {
+    await processWithAi({
+      userId,
+      organizationId,
+      workspaceId,
+      systemPrompt,
+      userPrompt,
+    });
+  } catch (err) {
+    console.error("Failed");
+  }
 
   console.log(stream);
 }
