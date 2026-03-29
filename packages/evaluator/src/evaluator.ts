5c31592f67001164a6937074f823b7503dbefa2c
Arav menon <aravmenon.ak@gmail.com>
2026-03-29 23:46:08 +0530

    feat : setup API integration of LLM for response

---
+++ packages/evaluator/src/evaluator.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+export const processWithAi = async ({
+  userId,
+  organizationId,
+  workspaceId,
+  systemPrompt,
+  userPrompt,
+}: any) => {
+  try {
+    const response = await axios.post(
+      "https://openrouter.ai/api/v1/chat/completions",
+      {
+        model: "arcee-ai/trinity-large-preview:free",
+        message: [
+          { role: "system", content: systemPrompt },
+          { role: "user", content: userPrompt },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+          "Content-Type": "application/json",
+          "HTTP-Referer": "http://localhost:4000",
+          "X-Title": "DevForces Evaluator",
+        },
+      },
+    );
+
+    const result = response.data.choices[0].message.content;
+    console.log(result);
+  } catch (err) {}
+};
