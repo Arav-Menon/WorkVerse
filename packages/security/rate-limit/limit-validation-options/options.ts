7c5391bc5b457003c81b38868d5cc3cd11f73ec6
Arav menon <aravmenon.ak@gmail.com>
2026-05-27 02:52:33 +0530

    refactor: remove specific rate limit plugins and standardize options in favor of a unified rate limit plugin implementation

--- packages/security/rate-limit/limit-validation-options/options.ts
+++ packages/security/rate-limit/limit-validation-options/options.ts
@@ -2,6 +2,8 @@ import type { client } from "@repo/redis";
 
 export interface AuthRateLimitOptions {
   redis?: typeof client;
+  global?: boolean;
+  hook?: string;
   max?: number;
   timeWindow?: number;
 }
@@ -9,5 +11,23 @@ export interface AuthRateLimitOptions {
 export interface RegisterOrgRateLimitOptions {
   redis?: typeof client;
   max?: number;
+  hook?: string;
+  global?: boolean;
+  timeWindow?: number;
+}
+
+export interface WorkspceOrgRateLimitOptions {
+  redis?: typeof client;
+  max?: number;
+  hook?: string;
+  global?: boolean;
+  timeWindow?: number;
+}
+
+export interface InviteOrgRateLimitOptions {
+  redis?: typeof client;
+  max?: number;
+  global?: boolean;
+  hook?: string;
   timeWindow?: number;
 }
