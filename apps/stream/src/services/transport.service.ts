6c99aa080c15cabc67a1ab23aaae603047001f21
Arav menon <aravmenon.ak@gmail.com>
2026-06-02 02:25:10 +0530

    feat: implement transport creation logic using TransportManager and update return types

--- apps/stream/src/services/transport.service.ts
+++ apps/stream/src/services/transport.service.ts
@@ -1,9 +1,17 @@
 import type { FastifyInstance } from "fastify";
-import type { TransportInput } from "@repo/schemas"
+import type { TransportInput } from "@repo/schemas";
+import { TransportManager } from "../managers/transport.manager.ts";
+import type { DtlsParameters, IceCandidate, IceParameters } from "mediasoup/types";
 
-export function createTransport(fastify: FastifyInstance, input: TransportInput) {
+export async function createTransport(fastify: FastifyInstance, input: TransportInput): Promise<{
+    id: string,
+    iceParameters: IceParameters,
+    iceCandidates: IceCandidate[],
+    dtlsParameters: DtlsParameters,
+}> {
     const { roomId, userId } = input;
 
+    if (!roomId || !userId) throw new Error("please provide roomId and userId");
 
-
+    return TransportManager.createTransport(roomId, userId);
 }
