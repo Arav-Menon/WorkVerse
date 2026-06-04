bc68f209aa2ea4b43a240cc93a6bb3c5b3f5c4e2
Arav menon <aravmenon.ak@gmail.com>
2026-06-04 21:24:22 +0530

    feat: implement WebRTC signaling relay server and add spatial proximity event triggers to space manager

--- apps/relay/src/signalling.ts
+++
@@ -1,9 +0,0 @@
-import { WebSocketServer } from "ws";
-
-const wss = new WebSocketServer({ port: 5000 });
-
-wss.on("connection", (socket) => {
-    socket.on("message", (data) => {
-        console.log(data.toString())
-    })
-})
