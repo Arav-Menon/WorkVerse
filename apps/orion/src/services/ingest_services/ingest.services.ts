12fa12752ad9ab301068a0b834941c84ea99da1a
Arav menon <aravmenon.ak@gmail.com>
2026-06-10 22:22:21 +0530

    refactor: expose planner logic through new evaluator package and integrate into ingest service

--- apps/orion/src/services/ingest_services/ingest.services.ts
+++ apps/orion/src/services/ingest_services/ingest.services.ts
@@ -1,5 +1,7 @@
 import type { ingestPromptBody } from "@repo/schemas";
 import { type FastifyInstance } from "fastify";
+import { planExecution } from "@repo/evaluator";
+import { get } from "http";
 
 export async function registerIngestPromptService(
   fastify: FastifyInstance,
@@ -16,66 +18,98 @@ export async function registerIngestPromptService(
     workspaceId,
     userPrompt,
     organizationId,
-    systemPrompt,
+    // systemPrompt,
     promptId,
     userId,
   } = input;
 
-  // have to write the thinging logic which execution tool to use mcp or n8n. remove this pushUserInboundPromt queue from here because it does decide blindly pushing to the queue
+  const jobStatusPayload2 = {
+    status: "queued",
+    userId,
+    userPrompt,
+    organizationId,
+    workspaceId,
+    promptId,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  };
+
+  console.log(jobStatusPayload2)
+
   try {
-    await fastify.redisProducer.pushUserInboundPrompt({
-      promptId,
-      userId,
-      organizationId,
-      workspaceId,
-      systemPrompt,
-      userPrompt,
-    });
 
-    const jobStatusPayload = {
-      status: "queued",
-      userId,
-      organizationId,
-      workspaceId,
-      promptId,
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
-    };
-
-    await Promise.all([
-      fastify.db.workflowJob.create({
-        data: {
-          id: promptId,
-          createdById: userId,
-          organizationId,
-          workspaceId,
-          title: promptId,
-          prompt: userPrompt,
-          systemPrompt,
-          status: "PENDING",
-        },
-      }),
-
-      fastify.cache.set(
-        `promptId:${promptId}`,
-        JSON.stringify(jobStatusPayload),
-        "EX",
-        3600,
-      ),
-    ]);
-
+    console.log("step:1")
+    const getTheIntent = await planExecution(userPrompt);
+    console.log("step:2")
+    console.log(getTheIntent)
     return {
       success: true,
-      id: promptId,
-      jobStatus: jobStatusPayload,
-    };
+      // statusCode: error.statusCode,
+      message: "DOne",
+      // error: error,
+    }
   } catch (error: any) {
-    console.log(error);
+    console.log("error from here", error);
     return {
       success: false,
       statusCode: error.statusCode,
       message: "Failed to send prompt in queue",
       error: error,
-    };
+    }
   }
+
+  // have to write the thinging logic which execution tool to use mcp or n8n. remove this pushUserInboundPromt queue from here because it does decide blindly pushing to the queue
+  // try {
+  //   await fastify.redisProducer.pushUserInboundPrompt({
+  //     promptId,
+  //     userId,
+  //     organizationId,
+  //     workspaceId,
+  //     systemPrompt,
+  //     userPrompt,
+  //   });
+  //
+  //   const jobStatusPayload = {
+  //     status: "queued",
+  //     userId,
+  //     organizationId,
+  //     workspaceId,
+  //     promptId,
+  //     createdAt: new Date().toISOString(),
+  //     updatedAt: new Date().toISOString(),
+  //   };
+  //
+  //   console.log(jobStatusPayload);
+  //
+  //   // await Promise.all([
+  //   //   fastify.db.workflowJob.create({
+  //   //     data: {
+  //   //       id: promptId,
+  //   //       createdById: userId,
+  //   //       organizationId,
+  //   //       workspaceId,
+  //   //       title: promptId,
+  //   //       prompt: userPrompt,
+  //   //       systemPrompt,
+  //   //       status: "PENDING",
+  //   //     },
+  //   //   }),
+  //   //
+  //   //   fastify.cache.set(
+  //   //     `promptId:${promptId}`,
+  //   //     JSON.stringify(jobStatusPayload),
+  //   //     "EX",
+  //   //     3600,
+  //   //   ),
+  //   // ]);
+  //
+  //   return {
+  //     success: true,
+  //     id: promptId,
+  //     jobStatus: jobStatusPayload,
+  //   };
+  // } catch (error: any) {
+  //   console.log(error);
+  //   };
+  // }
 }
