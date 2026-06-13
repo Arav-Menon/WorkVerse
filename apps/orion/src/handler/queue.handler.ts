3a5405549ccb33625d5bba5d6f56b65cf9ee999d
Arav menon <aravmenon.ak@gmail.com>
2026-06-13 22:51:59 +0530

    feat: implement conversational chat interface, establish event-driven architecture, and restructure service components

---
+++ apps/orion/src/handler/queue.handler.ts
@@ -0,0 +1,17 @@
+// import { chatQueue } from "@repo/queue";
+// import { planExecution } from "@repo/evaluator";
+
+
+// class QueueHandler {
+//     private {
+
+//     }
+
+//     CHATQUEUE() {
+
+//     }
+
+//     worklfowQueue() {
+
+//     }
+// }
