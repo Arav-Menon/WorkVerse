2f3d07c6c65394d6f0f1469bd07a0e46b773a647
Arav menon <aravmenon.ak@gmail.com>
2026-06-14 14:35:35 +0530

    feat: migrate to workflow schema, implement bulk insert worker, and add event bus subscription support

--- apps/forger/src/forger.ts
+++ apps/forger/src/forger.ts
@@ -1,16 +1,33 @@
 import { Worker } from "bullmq";
 import { connection } from "@repo/queue";
-import { chat } from "@repo/evaluator"
+import { chat } from "@repo/evaluator";
+import type { ChatCompletedEvent } from "@repo/events";
+import { EventBus } from "@repo/events";
+import { client } from "@repo/redis";
+import { insertBulk } from "../utils/bulkInsert";
 
 export const worker = new Worker('chat_response-queue', async (job) => {
     console.log("worker is started.....")
     const { workspaceId, userPrompt, organizationId, promptId, userId } = job.data;
 
     try {
-        console.log("reached to worker")
         const response = await chat(userPrompt);
-        console.log("get the response from the worker")
-        console.log(response)
+
+        const chatCompletePayload: ChatCompletedEvent = {
+            promptId,
+            userId,
+            organizationId,
+            workspaceId,
+            content: response,
+            status: "completed"
+        }
+
+        await client.set(`chat${promptId}:access`, JSON.stringify(chatCompletePayload), "EX", 86400);
+
+        const dbCheck = await insertBulk(chatCompletePayload) as any;
+
+        await EventBus.publish("chat_completed", chatCompletePayload);
+
         return {
             success: true,
             statusCode: 200,
@@ -24,10 +41,10 @@ export const worker = new Worker('chat_response-queue', async (job) => {
         };
     } catch (error: any) {
         return {
-            success: true,
+            success: false,
             statusCode: error.statusCode ?? 500,
             error
         }
     }
 
-}, { connection })
+}, { connection, concurrency: 100 })
