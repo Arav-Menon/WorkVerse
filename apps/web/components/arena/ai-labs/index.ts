f7e892fb92de58747c362c66c4ee8bd873f052d1
Arav menon <aravmenon.ak@gmail.com>
2026-06-19 23:55:10 +0530

    feat: redesign world aesthetics with warm color palette and updated player avatars

---
+++ apps/web/components/arena/ai-labs/index.ts
@@ -0,0 +1,8 @@
+export { AiLabsHeader } from './AiLabsHeader';
+export { AiLabsWorkspaceContext } from './AiLabsWorkspaceContext';
+export { AiLabsSuggestions } from './AiLabsSuggestions';
+export { AiLabsActivityFeed } from './AiLabsActivityFeed';
+export { AiLabsExecutions } from './AiLabsExecutions';
+export { AiLabsMemory } from './AiLabsMemory';
+export { AiLabsChat } from './AiLabsChat';
+export { AiLabsInput } from './AiLabsInput';
