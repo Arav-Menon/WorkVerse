ebd615c91d924b55d57a504538542df4c2c1006e
Arav menon <aravmenon.ak@gmail.com>
2026-06-21 22:43:18 +0530

    feat: add organization description field and implement organization and invite management API layer

--- apps/web/lib/api/endpoints.ts
+++ apps/web/lib/api/endpoints.ts
@@ -4,9 +4,18 @@ export const API_ENDPOINTS = {
         LOGIN : "/api/v1/auth/login"
     },
     ORGANIZATION: {
-        REGISTER : "/api/v1/register-organization"
+        REGISTER : "/api/v1/register-organization",
+        GET_ALL : "/api/v1/organizations",
+        GET_BY_ID : (orgId : string) => `/api/v1/organizations/${orgId}`,
+        GET_BY_SLUG : (slug : string) => `/api/v1/organizations/slug/${slug}`,
+        INVITE : (orgId : string) => `/api/v1/generate-invite-link/${orgId}`,
+        INVITE_VALIDATE : (token : string) => `/api/v1/invite/${token}`,
+        INVITE_ACCEPT : "/api/v1/accept-invite",
     },
     WORKSPACE: {
         CREATE: (orgId : string) => `/api/v1/register-workspace/${orgId}`,
+    },
+    PROFILE: {
+        ME: "/api/v1/profile/me",
     }
 }
