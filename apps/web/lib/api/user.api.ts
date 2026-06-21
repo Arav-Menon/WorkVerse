f05f6c39368efdac90c0e1b9fc1389e47aa7bbe9
Arav menon <aravmenon.ak@gmail.com>
2026-06-21 22:43:45 +0530

    feat: add description field to organization schema and refactor user authentication API methods

--- apps/web/lib/api/user.api.ts
+++ apps/web/lib/api/user.api.ts
@@ -1,15 +1,11 @@
 import { apiClient } from "@/lib/api/client";
 import { API_ENDPOINTS } from "@/lib/api/endpoints";
 
-interface User {
-    name : string,
-    email : string,
-    password : string
+export async function registerUser(name : string, email : string, password : string) {
+    const res = await apiClient.post(API_ENDPOINTS.USER.REGISTER, {name, email, password});
+    return res.data.data.token;
 }
-
-export async function authUser(data : User) {
-    const res = await apiClient.post(API_ENDPOINTS.USER.REGISTER, data);
-    console.log(res.data)
-    return res.data;
+export async function loginUser(email : string, password : string) {
+    const res = await apiClient.post(API_ENDPOINTS.USER.LOGIN, { email, password});
+    return res.data.data.token;
 }
-
