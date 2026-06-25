2a0c594c4610207855198de18272111be0168d8a
Arav menon <aravmenon.ak@gmail.com>
2026-06-25 23:35:17 +0530

    fix: remove unused components from SpaceArenaPage to streamline rendering

--- apps/web/app/space/[spaceId]/page.tsx
+++ apps/web/app/space/[spaceId]/page.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { ArenaHUD } from '../../../components/arena/ArenaHUD';
 import { ArenaWrapper } from '../../../components/arena/ArenaWrapper';
-import { BottomNavBar } from '../../../components/arena/BottomNavBar';
-import { AiLabsPanel } from '../../../components/arena/AiLabsPanel';
 
 interface PageProps {
   params: Promise<{ spaceId: string }>;
@@ -14,9 +11,6 @@ export default async function SpaceArenaPage({ params }: PageProps) {
   return (
     <div className="relative w-full h-screen overflow-hidden" style={{ background: '#050505' }}>
       <ArenaWrapper spaceId={spaceId} />
-      <ArenaHUD spaceId={spaceId} />
-      <AiLabsPanel />
-      <BottomNavBar />
     </div>
   );
 }
