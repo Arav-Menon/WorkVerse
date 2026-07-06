21425d78ac1aabf05723da56d951e8c9fbe484ee
Arav menon <aravmenon.ak@gmail.com>
2026-07-06 18:14:51 +0530

    fix: update createRedisSubscriber to handle redisConfig type check

--- packages/redis/src/connection/subscriber.ts
+++ packages/redis/src/connection/subscriber.ts
@@ -1,6 +1,9 @@
 import Redis from "ioredis";
 import { redisConfig } from "./config";
 
-export const createRedisSubscriber = () => new Redis(redisConfig);
+export const createRedisSubscriber = () =>
+  typeof redisConfig === "string"
+    ? new Redis(redisConfig)
+    : new Redis(redisConfig);
 
 export const subscriberRedis = createRedisSubscriber();
