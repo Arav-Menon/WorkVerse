9f765eef20bcc10c8518b88aee884332d51cc107
Arav menon <aravmenon.ak@gmail.com>
2026-07-06 08:11:56 +0530

    refactor: remove console commit form space manager

--- apps/space/src/services/space-manager.ts
+++ apps/space/src/services/space-manager.ts
@@ -159,7 +159,6 @@ export class spaceManager {
     }
 
     async moveClient(workSpaceId: string, userId: string, position: { x: number, y: number }) {
-        // console.log("some")
         const raw = await client.hget(`space:${workSpaceId}:users`, userId);
         if (!raw) return;
         let spaceUser: SpaceUser;
